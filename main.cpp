d9b241ece2f7af8a480880a19b09ee9a1e3b39fd
reemehab <60987184+reemehab@users.noreply.github.com>
2022-05-26 22:14:27 +0200

    Lines
    Lines Algorithms "DDA, BresenhamAlgo, parametric"

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@
 
 #include <tchar.h>
 #include <windows.h>
-
+#include <iostream>
+using namespace std;
 /*  Declare Windows procedure  */
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
@@ -52,19 +53,19 @@ int WINAPI WinMain(HINSTANCE hThisInstance,
 
     /* The class is registered, let's create the program*/
     hwnd = CreateWindowEx(
-        0,                               /* Extended possibilites for variation */
-        szClassName,                     /* Classname */
-        _T("Computer Graphics Project"), /* Title Text */
-        WS_OVERLAPPEDWINDOW,             /* default window */
-        CW_USEDEFAULT,                   /* Windows decides the position */
-        CW_USEDEFAULT,                   /* where the window ends up on the screen */
-        544,                             /* The programs width */
-        375,                             /* and height in pixels */
-        HWND_DESKTOP,                    /* The window is a child-window to desktop */
-        CreateMenus(),                   /* Menu bar */
-        hThisInstance,                   /* Program Instance handler */
-        NULL                             /* No Window Creation data */
-    );
+               0,                               /* Extended possibilites for variation */
+               szClassName,                     /* Classname */
+               _T("Computer Graphics Project"), /* Title Text */
+               WS_OVERLAPPEDWINDOW,             /* default window */
+               CW_USEDEFAULT,                   /* Windows decides the position */
+               CW_USEDEFAULT,                   /* where the window ends up on the screen */
+               544,                             /* The programs width */
+               375,                             /* and height in pixels */
+               HWND_DESKTOP,                    /* The window is a child-window to desktop */
+               CreateMenus(),                   /* Menu bar */
+               hThisInstance,                   /* Program Instance handler */
+               NULL                             /* No Window Creation data */
+           );
 
     /* Make the window visible on the screen */
     ShowWindow(hwnd, nCmdShow);
@@ -81,13 +82,160 @@ int WINAPI WinMain(HINSTANCE hThisInstance,
     /* The program return-value is 0 - The value that PostQuitMessage() gave */
     return messages.wParam;
 }
+int Round(double x)
+{
+    return (int)(x + 0.5);
+}
+
+struct point
+{
+    int x,y;
+
+};
+void swap(double x1,double y1, double x2, double y2)
+{
+    double temp=0;
+    temp=x1;
+    x2=x1;
+    x1=temp;
+    temp=y1;
+    y1=y2;
+    y2=temp;
+}
+void lineDDA(HDC hdc,int x1, int y1, int x2, int y2, COLORREF c)
+{
+    int dx=x2-x1;
+    int dy=y2-y1;
+    if(abs(dy)<=abs(dx))///slope < 1
+    {
+    if(x1>x2)
+        {
+            swap(x1,y1,x2,y2);
+        }
+        int x=x1;
+        double y=y1;
+        double m=(double)dy/dx;
+        SetPixel(hdc,x,y1,c);
+        while(x<x2)
+        {
+            x++;
+            y+=m;
+            SetPixel(hdc,x,Round(y),c);
+
+        }
+
+    }
+    else ///slope > 1
+    {
+        if(y1>y2)
+        {
+            swap(x1,y1,x2,y2);
+        }
+        double x=x1;
+        int y= y2;
+        double minV=dx/dy;
+        SetPixel(hdc,x1,y1,c);
+        while(y<y2)
+        {
+            y++;;
+            x+=minV;
+            SetPixel(hdc,Round(x),y,c);
+        }
+    }
+
+
+}
+void MidPointLine(HDC hdc,int x1, int y1, int x2, int y2,COLORREF c)
+{   cout<<"X1 "<<x1<<" Y1 "<<y1<<"X2"<<x2<<"Y2"<<y2<<endl;
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+
+    if(dy<=dx) ///slope  <1
+    {
+        if(x1>x2)
+        {
+            swap(x1,y1,x2,y2);
+        }
+
+        int d = dx - 2*dy;
+        int x = x1, y = y1;
+        int d1=-2*(dx-dy);
+        int d2=-2*dy;
+        SetPixel(hdc,x,y,c);
+        while (x < x2)
+        {
+            if (d <= 0)
+            {
+                x++;
+                y++;
+                d+=d1;
+            }
+            else
+            {
+                d += d2;
+                x++;
+            }
+
+            SetPixel(hdc,x,y,c);
+        }
+    }
+    else if(dx<dy)  /// slope > 1
+    {
+        int d = 2*dx - dy;
+        if(y1>y2)
+        {
+            swap(x1,y1,x2,y2);
+        }
+        int x = x1, y = y1;
+        while(y1<y2)
+        {
+            int d=2*dx-dy;
+            int d1=2*dx;
+            int d2=2*dx-2*dy;
+            SetPixel(hdc,x,y,c);
+            while(y<y2)
+            {
+                if (d <= 0)
+                {
+                    y++;
+                    d+=d1;
+                }
+                else
+                {
+                    d += d2;
+                    x++;
+                    y++;
+                }
+
+                SetPixel(hdc,x,y,c);
+
+
+            }
+
+        }
+    }
+}
+void paremetricLine(HDC hdc ,double x1, double y1, double x2, double y2, COLORREF c)
+{   double x,y;
+    for(double t=0; t<1 ; t+=0.0001)
+    {
+        x=x1+t*(x2-x1);
+        y=y1+t*(y2-y1);
+        SetPixel(hdc,Round(x),Round(y),c);
+    }
+}
 
 /*  This function is called by the Windows function DispatchMessage()  */
-
+int index=1;
+HDC hdc;
+int Xc, Yc, Y1, Y2, X1, X2,R1,R2;
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message) /* handle the messages */
     {
+    case WM_LBUTTONDBLCLK:
+        hdc = GetDC(hwnd);
+        break;
     case WM_DESTROY:
         PostQuitMessage(0); /* send a WM_QUIT to the message queue */
         break;
@@ -110,7 +258,12 @@ HMENU CreateMenus()
     AppendMenuW(hMenu, MF_STRING, IDM_FILE_OPEN, L"&Open");
     AppendMenuW(hMenu, MF_SEPARATOR, 0, NULL);
     AppendMenuW(hMenu, MF_STRING, IDM_FILE_QUIT, L"&Quit");
-
     AppendMenuW(hMenubar, MF_POPUP, (UINT_PTR)hMenu, L"&File");
+
+
+    AppendMenuW(hMenu, MF_STRING, IDM_FILE_NEW, L"&midPoint");
+    AppendMenuW(hMenu, MF_STRING, IDM_FILE_OPEN, L"&DDA");
+    AppendMenuW(hMenu, MF_STRING, IDM_FILE_QUIT, L"&Parmetric");
+    AppendMenuW(hMenubar, MF_POPUP, (UINT_PTR)hMenu, L"&Line");
     return hMenubar;
 }
